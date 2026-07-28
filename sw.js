1a24a5d0250c6b2d9706d976c74fe3074632106f
rob83dh <robert_d_h@hotmail.com>
2026-07-28 17:14:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Lagrer oppskriftene på enheten, slik at appen virker uten nett. */
-var CACHE = 'oppskrifter-v15';
+var CACHE = 'oppskrifter-v16';
 var FILES = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', function(e){
